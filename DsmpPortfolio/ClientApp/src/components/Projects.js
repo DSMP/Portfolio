f1a070ca52f5efb24acc044677bbe887292aab2d
Damian Smug <smugdamian@gmail.com>
2019-11-16 00:47:25 +0000

    init

---
+++ DsmpPortfolio/ClientApp/src/components/Projects.js
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react';
+
+export class Projects extends Component {
+    render() {
+        return (
+            <div style={{ background: "yellow" }}>
+                <div style={{ background: "red" }}>
+                    hello there
+                </div>
+                <div style={{ background: "blue" }}>
+                    hello there
+                </div>
+            </div>
+        )
+    }
+}
