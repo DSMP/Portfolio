353fab5284b0673ac429e40ada3b5357af229d41
Damian Smug <smugdamian@gmail.com>
2019-11-19 16:32:22 +0000

    add: separate project component from projects component, and pass properties

---
+++ DsmpPortfolio/ClientApp/src/components/Project.js
@@ -0,0 +1,17 @@
+﻿import React, { Component } from 'react';
+import './Project.css';
+
+export class Project extends Component {
+    render() {
+        return (
+            <div class="project">
+                <div class="image">
+                    <img src={this.props.img} />
+                </div>
+                <div class="paragraf">
+                    <p>{this.props.paragraph}</p>
+                </div>
+            </div>
+        )
+    }
+}
--- DsmpPortfolio/ClientApp/src/components/Projects.js
+++ DsmpPortfolio/ClientApp/src/components/Projects.js
@@ -1,26 +1,14 @@
 ﻿import React, { Component } from 'react';
+import { Project } from './Project';
 import './Projects.css';
 
 export class Projects extends Component {
     render() {
         return (
             <div class="projects">
-                <div class="project">
-                    <div class="image">
-                        <img src="https://alebank.pl/wp-content/uploads/2019/10/IT-AdobeStock_200146317-autor-Gorodenkoff.jpg"/>
-                    </div>
-                    <div class="paragraf">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac.</p>
-                    </div>
-                </div>
-                <div class="project">
-                    <div class="image">
-                        <img />
-                    </div>
-                    <div class="paragraf">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac.</p>
-                    </div>
-                </div>
+                <Project
+                    img={"https://alebank.pl/wp-content/uploads/2019/10/IT-AdobeStock_200146317-autor-Gorodenkoff.jpg"}
+                    paragraph="lorem ipsum..."/>
             </div>
         )
     }
