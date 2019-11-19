61fecbad2a89c080c80e2942de80900567ae3420
Damian Smug <smugdamian@gmail.com>
2019-11-19 17:01:53 +0000

    add: methods for fetch and populate data of projects

--- DsmpPortfolio/ClientApp/src/components/Projects.js
+++ DsmpPortfolio/ClientApp/src/components/Projects.js
@@ -3,13 +3,37 @@ import { Project } from './Project';
 import './Projects.css';
 
 export class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { projects: [], isLoading: true };
+    }
+
+    componentDidMount() {
+        this.populateProjectsData();
+    }
+
+    static renderProjects(projects) {
+        return(
+            <Project
+                img={"https://alebank.pl/wp-content/uploads/2019/10/IT-AdobeStock_200146317-autor-Gorodenkoff.jpg"}
+                paragraph="lorem ipsum..." />
+        )
+    }
+
     render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Projects.renderProjects(this.state.projects);
+
         return (
             <div class="projects">
-                <Project
-                    img={"https://alebank.pl/wp-content/uploads/2019/10/IT-AdobeStock_200146317-autor-Gorodenkoff.jpg"}
-                    paragraph="lorem ipsum..."/>
+                {contents}
             </div>
         )
+    }
+    async populateProjectsData() {
+        const response = await fetch('projects');
+        const data = await response.json();
+        this.setState({ projects: data, loading: false });
     }
 }
