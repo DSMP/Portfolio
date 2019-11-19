edf1c409249cea7f6c1c9241f54abfc727c4e11c
Damian Smug <smugdamian@gmail.com>
2019-11-19 14:42:24 +0000

    add: paragraph and img

--- DsmpPortfolio/ClientApp/src/components/Projects.js
+++ DsmpPortfolio/ClientApp/src/components/Projects.js
@@ -1,14 +1,25 @@
 ﻿import React, { Component } from 'react';
+import './Projects.css';
 
 export class Projects extends Component {
     render() {
         return (
-            <div style={{ background: "yellow" }}>
-                <div style={{ background: "red" }}>
-                    hello there
+            <div class="projects">
+                <div class="project">
+                    <div class="image">
+                        <img src="https://alebank.pl/wp-content/uploads/2019/10/IT-AdobeStock_200146317-autor-Gorodenkoff.jpg"/>
+                    </div>
+                    <div class="paragraf">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac.</p>
+                    </div>
                 </div>
-                <div style={{ background: "blue" }}>
-                    hello there
+                <div class="project">
+                    <div class="image">
+                        <img />
+                    </div>
+                    <div class="paragraf">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac.</p>
+                    </div>
                 </div>
             </div>
         )
