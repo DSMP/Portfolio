e9a2c17ea10bf00be660da2aa221584bca53fbec
Damian Smug <smugdamian@gmail.com>
2019-11-25 20:05:49 +0000

    add: some colors to home page

--- DsmpPortfolio/ClientApp/src/components/Home.js
+++ DsmpPortfolio/ClientApp/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -6,28 +7,28 @@ export class Home extends Component {
   render () {
       return (
           <div>
-              <div class="jumbotron">
+              <div class="jumbotron first">
                   <h1 class="display-3 text-monospace text-center">
                       Looking for programmer?<br />
                       Maybe some solution...
               </h1>
               </div>
-              <div class="jumbotron">
+              <div class="jumbotron second">
                   <h1 class="display-3 text-monospace text-center">
                       Mobile App?
               </h1>
               </div>
-              <div class="jumbotron">
+              <div class="jumbotron third">
                   <h1 class="display-3 text-monospace text-center">
                       Web App?
               </h1>
               </div>
-              <div class="jumbotron">
+              <div class="jumbotron fourth">
                   <h1 class="display-3 text-monospace text-center">
                       Desktop app?
               </h1>
               </div>
-              <div class="jumbotron">
+              <div class="jumbotron fifth">
                   <h1 class="display-3 text-monospace text-center">
                       Other solution?
               </h1>
