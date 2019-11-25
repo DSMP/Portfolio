976f8e2b90d1d641d7c45b37a78324c82ce2978c
Damian Smug <smugdamian@gmail.com>
2019-11-25 21:43:52 +0000

    add: buttons

--- DsmpPortfolio/ClientApp/src/components/Home.js
+++ DsmpPortfolio/ClientApp/src/components/Home.js
@@ -2,38 +2,53 @@ import React, { Component } from 'react';
 import './Home.css';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-      return (
-          <div>
-              <div class="jumbotron first">
-                  <h1 class="display-3 text-monospace text-center">
-                      Looking for programmer?<br />
-                      Maybe some solution...
+    render() {
+        return (
+            <div>
+                <div class="container-fluid first container-padding">
+                    <h1 class="display-5 text-monospace text-center">
+                        Looking for solution for your company?
+                    </h1>
+                    <div class="row text-center myrow">
+                        <div class="col">
+                            <button type="button" class="btn btn-outline-primary btn-size">Solutions</button>
+                        </div>
+                        <div class="col">
+                            <button type="button" class="btn btn-outline-primary btn-size">Web</button>
+                        </div>
+                    </div>
+                    <div class="row text-center myrow">
+                        <div class="col">
+                            <button type="button" class="btn btn-outline-primary btn-size">Mobile</button>
+                        </div>
+                        <div class="col">
+                            <button type="button" class="btn btn-outline-primary btn-size">Desktop</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="jumbotron second">
+                    <h1 class="display-3 text-monospace text-center">
+                        Mobile App?
               </h1>
-              </div>
-              <div class="jumbotron second">
-                  <h1 class="display-3 text-monospace text-center">
-                      Mobile App?
+                </div>
+                <div class="jumbotron third">
+                    <h1 class="display-3 text-monospace text-center">
+                        Web App?
               </h1>
-              </div>
-              <div class="jumbotron third">
-                  <h1 class="display-3 text-monospace text-center">
-                      Web App?
+                </div>
+                <div class="jumbotron fourth">
+                    <h1 class="display-3 text-monospace text-center">
+                        Desktop app?
               </h1>
-              </div>
-              <div class="jumbotron fourth">
-                  <h1 class="display-3 text-monospace text-center">
-                      Desktop app?
+                </div>
+                <div class="jumbotron fifth">
+                    <h1 class="display-3 text-monospace text-center">
+                        Other solution?
               </h1>
-              </div>
-              <div class="jumbotron fifth">
-                  <h1 class="display-3 text-monospace text-center">
-                      Other solution?
-              </h1>
-              </div>
-          </div>
-    );
-  }
+                </div>
+            </div>
+        );
+    }
 }
